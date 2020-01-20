44c286682ad019212cf37bd9014f310c82f4d15b
Jialin Tian <i@kenshin54.me>
2020-01-20 13:14:27 +0800

    Remove requirements.txt and update setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
 from setuptools import setup, find_packages
 
+requires = [
+    'awscli>=1.17.5,<2.0',
+    'botocore>=1.14.5,<2.0',
+]
+test_requirements = [
+    'mock>=3.0',
+]
+
 setup(
     name='aws-simple-mfa',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
+    description='Simple MFA plugin for AWS CLI',
+    author='kenshin54',
+    author_email='i@kenshin54.me',
+    url='https://github.com/kenshin54/aws-simple-mfa',
+    keywords=['awscli', 'plugin', 'mfa'],
+    install_requires=requires,
+    tests_require=test_requirements,
 )
